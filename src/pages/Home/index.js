7f6b00aa3ffbbec21d160696cde6fc5509a799e1
luccaroli <luccaroli20@gmail.com>
2020-09-19 12:33:44 -0300

    search products api with react hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdShoppingCart } from 'react-icons/md'
 
+import api from '../../services/api'
+import { formatPrice } from '../../utils/format'
 import { ProductList } from './styles';
 
 
+
 function Home() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get('products')
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price)
+      }))
+      setProducts(data)
+    }
+
+    loadProducts()
+
+  }, [])
+
   return (
     <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adaption-easy-v2-masculino/06/KTM-0100-006/KTM-0100-006_zoom2.jpg?ts=1584370742&ims=326x" alt="Tênis"/>
-        <strong>Tênis Nike</strong>
-        <span>R$ 192,90</span>
+      { products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title}/>
+          <strong> {product.title} </strong>
+          <span> {product.priceFormatted} </span>
 
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
+          <button type="button">
+            <div>
+              <MdShoppingCart size={16} color="#fff" /> 3
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adaption-easy-v2-masculino/06/KTM-0100-006/KTM-0100-006_zoom2.jpg?ts=1584370742&ims=326x" alt="Tênis"/>
-        <strong>Tênis Nike</strong>
-        <span>R$ 192,90</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adaption-easy-v2-masculino/06/KTM-0100-006/KTM-0100-006_zoom2.jpg?ts=1584370742&ims=326x" alt="Tênis"/>
-        <strong>Tênis Nike</strong>
-        <span>R$ 192,90</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adaption-easy-v2-masculino/06/KTM-0100-006/KTM-0100-006_zoom2.jpg?ts=1584370742&ims=326x" alt="Tênis"/>
-        <strong>Tênis Nike</strong>
-        <span>R$ 192,90</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adaption-easy-v2-masculino/06/KTM-0100-006/KTM-0100-006_zoom2.jpg?ts=1584370742&ims=326x" alt="Tênis"/>
-        <strong>Tênis Nike</strong>
-        <span>R$ 192,90</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adaption-easy-v2-masculino/06/KTM-0100-006/KTM-0100-006_zoom2.jpg?ts=1584370742&ims=326x" alt="Tênis"/>
-        <strong>Tênis Nike</strong>
-        <span>R$ 192,90</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      )) }
+      
     </ProductList>
   )
 }
